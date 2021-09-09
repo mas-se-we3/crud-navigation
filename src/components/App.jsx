928d14c363d0aaab3776bda088b474862227a618
Tobias Büchel <tobi.buechel@gmail.com>
2021-09-09 11:40:18 +0200

    Define app routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Redirect, Route, Switch } from 'react-router'
 import './App.css'
+import { Overview } from './pages/Overview'
+import { UserDetails } from './pages/UserDetails'
 import { Appbar } from './widgets/Appbar'
 
 export const App = () => (
 	<div>
 		<Appbar />
 		<main className="app__main">
-			{/* TODO: Definiere Routes mit <Switch>, <Route> und optional <Redirect> */}
+			<Switch>
+				<Route exact path="/" component={Overview} />
+				<Route exact path="/users/:id" component={UserDetails} />
+				<Route exact path="/new-user" component={UserDetails} />
+				<Redirect to="/" />
+			</Switch>
 		</main>
 	</div>
 )
